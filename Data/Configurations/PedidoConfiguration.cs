7daab1ed7a63314339651c0888ed9d3980184825
matheusfconde <matheusf.conde@gmail.com>
2022-07-15 20:07:24 -0300

    Utilização do Migration para criação do banco de dados SQL

--- Data/Configurations/PedidoConfiguration.cs
+++ Data/Configurations/PedidoConfiguration.cs
@@ -10,10 +10,10 @@ namespace CursoEFCore.Data.Configurations
         {
             builder.ToTable("Pedidos");
             builder.HasKey(p => p.Id);
-            builder.Property(p => p.IniciadoEm).HasDefaultValue("GETDATE()").ValueGeneratedOnAdd();
+            builder.Property(p => p.IniciadoEm).HasDefaultValueSql("GETDATE()").ValueGeneratedOnAdd();
             builder.Property(p => p.Status).HasConversion<string>();
             builder.Property(p => p.TipoFrete).HasConversion<int>();
-            builder.Property(p => p.Observacao).HasColumnType("VARCHAR(512");
+            builder.Property(p => p.Observacao).HasColumnType("VARCHAR(512)");
 
             builder.HasMany(p => p.Itens)
                 .WithOne(p => p.Pedido)
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using CursoEFCore.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CursoEF
 {
@@ -6,6 +9,15 @@ namespace CursoEF
     {
         static void Main(string[] args)
         {
+            using var db = new ApplicationContext();
+            //db.Database.Migrate(); verificar andamento da migração
+            var existe = db.Database.GetPendingMigrations().Any();
+
+            if (existe)
+            {
+                Console.WriteLine("existe validação pendente!");
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
