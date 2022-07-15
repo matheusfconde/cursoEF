c1a3b2d30f3722c37b022e6708754d308be076ec
matheusfconde <matheusf.conde@gmail.com>
2022-07-15 17:01:22 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CursoEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
